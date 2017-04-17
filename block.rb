6095d048a03b99517374fa1edd52a91b2d661247
Patrick Brodie <patrick@shoptiques.com>
2017-04-16 21:06:16 -0400

    add tap

---
+++ block.rb
@@ -0,0 +1,14 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Block < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/ptbrodie/block/archive/v0.0.1.tar.gz"
+  sha256 "a73f857583dc04c406dfc50359b43b80c8795987686882f6f76d7d985ee04820"
+
+  def install
+    bin.install "block.sh"
+  end
+end
